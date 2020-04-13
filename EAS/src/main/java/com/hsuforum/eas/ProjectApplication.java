49fb60cf519d830cd575e223db02deb6ae657511
Marvin <Marvin@WINDESKTOP1.hsuforum.com>
2020-04-13 12:29:43 +0800

    Init version 2.0.0

---
+++ EAS/src/main/java/com/hsuforum/eas/ProjectApplication.java
@@ -0,0 +1,17 @@
+package com.hsuforum.eas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "com.hsuforum.eas")
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class} )
+public class ProjectApplication  extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
